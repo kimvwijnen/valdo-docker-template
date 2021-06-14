dea87fc62b890a96ca959ec8f84cd376501e7d28
kimvwijnen <kimvwijnen@gmail.com>
2021-06-14 18:22:33 +0200

    Enable multiple input modalities
    Added the option to indicate in self.input_modalities which modalities the method takes as input (can be one or multiple). Overloaded _load_input_image to load in a list of images to pass to predict. Needed to skip UniquePathIndicesValidator as this errors when multiple images (modalities) are present in the input folder per subject (which is the case when running with multiple modalities as input)

--- process.py
+++ process.py
@@ -10,10 +10,18 @@ from evalutils.validators import (
 
 # added imports
 import tensorflow as tf
+from typing import Tuple, List
+from pathlib import Path
+import re
+
+from evalutils.io import (ImageLoader, SimpleITKLoader)
+from evalutils.validators import DataFrameValidator, UniqueImagesValidator
 
 
 class Findpvs(SegmentationAlgorithm):
     def __init__(self):
+        self.input_modalities = ['T1', 'T2', 'FLAIR']
+        self.first_modality = self.input_modalities[0]
         super().__init__(
             validators=dict(
                 input_image=(
@@ -21,7 +29,12 @@ class Findpvs(SegmentationAlgorithm):
                     UniquePathIndicesValidator(),
                 )
             ),
+            # indicate with regex which image to load as input, e.g. T1 scan
+            file_filters={'input_image':
+                          re.compile("/input/sub-.*_space-.*_desc-masked_%s.nii.gz" % self.first_modality)}
         )
+        # Skip UniquePathIndicesValidator, because this will error when there are multiple images for the same subject
+        self._validators: Dict[str, Tuple[DataFrameValidator, ...]] = (dict(input_image=(UniqueImagesValidator(),)))
 
         print("==> Initializing model")
 
@@ -38,19 +51,51 @@ class Findpvs(SegmentationAlgorithm):
 
         print("==> Weights loaded")
 
+    def _load_input_image(self, *, case) -> Tuple[List[SimpleITK.Image], Path]:
+        input_image_file_path = case["path"]
+
+        input_image_file_loader = self._file_loaders["input_image"]
+        if not isinstance(input_image_file_loader, ImageLoader):
+            raise RuntimeError(
+                "The used FileLoader was not of subclass ImageLoader"
+            )
+        input_images = []
+
+        # input_image = input_image_file_loader.load_image(input_image_file_path)
+        # input_images.append(input_image)
+
+        # add_image_path = input_image_file_path.parent / Path(input_image_file_path.name.replace('T1.nii.gz',
+        #                                                                                         'FLAIR.nii.gz'))
+        # input_images.append(input_image_file_loader.load_image(add_image_path))
+
+        # Load the image(s) for this case
+        for modality in self.input_modalities:
+            # Load all input images, e.g. T1, T2 and FLAIR
+            scan_name = Path(input_image_file_path.name.replace('%s.nii.gz' % self.first_modality,
+                                                                '%s.nii.gz' % modality))
+            modality_path = input_image_file_path.parent / scan_name
+            print(modality_path)
+            input_images.append(input_image_file_loader.load_image(modality_path))
+
+        # Check that it is the expected image
+        if input_image_file_loader.hash_image(input_images[0]) != case["hash"]:
+            raise RuntimeError("Image hashes do not match")
+
+        return input_images, input_image_file_path
+
     def process_case(self, *, idx, case):
         # Load and test the image for this case
-        input_image, input_image_file_path = self._load_input_image(case=case)
+        input_images, input_image_file_path = self._load_input_image(case=case)
 
-        # Segment nodule candidates
-        segmented_nodules = self.predict(input_image=input_image)
+        # Segment case
+        segm_prediction = self.predict(input_images=input_images)
 
         # Write resulting segmentation to output location
         output_name = Path(input_image_file_path.name.split("desc-masked_")[0] + "desc-prediction.nii.gz")
         segmentation_path = self._output_path / output_name
         if not self._output_path.exists():
             self._output_path.mkdir()
-        SimpleITK.WriteImage(segmented_nodules, str(segmentation_path), True)
+        SimpleITK.WriteImage(segm_prediction, str(segmentation_path), True)
 
         # Write segmentation file path to result.json for this case
         return {
@@ -63,31 +108,37 @@ class Findpvs(SegmentationAlgorithm):
             "error_messages": [],
         }
 
-    def predict(self, *, input_image: SimpleITK.Image) -> SimpleITK.Image:
+    def predict(self, *, input_images: List[SimpleITK.Image]) -> SimpleITK.Image:
+        input_list = []
 
-        # image to numpy array
-        image = SimpleITK.GetArrayFromImage(input_image)
-        print('Image shape:')
-        print(image.shape)
+        for img in input_images:
+            # image to numpy array
+            image = SimpleITK.GetArrayFromImage(img)
+            print('Image shape:')
+            print(image.shape)
 
-        # --> Preprocess image
-        image = np.array(image, dtype=np.float32)
+            # --> Preprocess image
+            image = np.array(image, dtype=np.float32)
 
-        # normalize image (rescale)
-        min_data = np.amin(image)
-        max_data = np.amax(image)
-        image = (image - min_data) * 1. / (max_data - min_data)
+            # normalize image (rescale)
+            min_data = np.amin(image)
+            max_data = np.amax(image)
+            image = (image - min_data) * 1. / (max_data - min_data)
 
-        # convert (z,y,x) to (x,y,z)
-        image = np.moveaxis(image, [0, 2], [2, 0])
-        print('Image shape after conversion:')
-        print(image.shape)
+            # convert (z,y,x) to (x,y,z)
+            image = np.moveaxis(image, [0, 2], [2, 0])
+            print('Image shape after conversion:')
+            print(image.shape)
 
-        # add batch axis and channel axis (at front and end for tensorflow)
-        image = np.expand_dims(np.expand_dims(image, axis=-1), axis=0)
+            # add batch axis and channel axis (at front and end for tensorflow)
+            image = np.expand_dims(np.expand_dims(image, axis=-1), axis=0)
+            input_list.append(image)
+
+        # In TF the last axis contains the channels
+        input_channels = np.concatenate(input_list, axis=-1)
 
         # --> Apply NN on image
-        out = np.squeeze(self.model.predict(image, batch_size=1, verbose=1))
+        out = np.squeeze(self.model.predict(input_channels, batch_size=1, verbose=1))
         print('Image shape prediction:')
         print(out.shape)
 
@@ -97,7 +148,7 @@ class Findpvs(SegmentationAlgorithm):
         print('Image shape before saving:')
         print(out.shape)
         out = SimpleITK.GetImageFromArray(out)
-        out.CopyInformation(input_image)
+        out.CopyInformation(input_images[0])
 
         print("==> Prediction done")
         # Prediction is saved in superclass SegmentationAlgorithm, in the process_case function
