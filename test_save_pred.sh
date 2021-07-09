ed17d94958a91dd22413ad629a04c887c6ac93ea
kimvwijnen <kimvwijnen@gmail.com>
2021-07-09 19:05:07 +0200

    Run Docker as user to test it
    By adding the command line argument -u $UID:$GROUPS the docker is run as the current user. This way there is no problem with file permissions when saving the output in the output folder

--- test_save_pred.sh
+++ test_save_pred.sh
@@ -4,19 +4,16 @@ SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 
 ./build.sh
 
-mkdir -p ${SCRIPTPATH}/output/images/
-# docker is run with user "algorithm" (see DockerFile), so need to set folder permissions
-# to allow any user to write to the output folder
-chmod 777 -R ${SCRIPTPATH}/output/
-
 docker run --rm --runtime nvidia --memory=10g --gpus="device=0" \
        -v ${SCRIPTPATH}/test/:/input/ -v ${SCRIPTPATH}/output/:/output/ \
-       findpvs
+       -u $UID:$GROUPS findpvs
+       #TODO change findpvs with teamname
 
 docker run --rm \
         -v ${SCRIPTPATH}/output/:/output/ \
         -v ${SCRIPTPATH}/test/:/input/ \
-        python:3.7-slim python -c "import json, sys; f1 = json.load(open('/output/results.json')); f2 = json.load(open('/input/expected_output.json')); sys.exit(f1 != f2);"
+        -u $UID:$GROUPS python:3.7-slim python -c "import json, sys; f1 = json.load(open('/output/results.json')); f2 = json.load(open('/input/expected_output.json')); sys.exit(f1 != f2);"
+        #TODO change findpvs with teamname
 
 if [ $? -eq 0 ]; then
     echo "Tests successfully passed..."
